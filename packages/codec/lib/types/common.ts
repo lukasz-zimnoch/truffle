2a95513ddfb6b7de05cfdd32a25128037e55fd67
g. nicholas d'andrea <nick@gnidan.org>
2019-10-03 20:29:03 -0400

    Move UnknownUserDefinedTypeError to types/common

--- packages/codec/lib/types/common.ts
+++ packages/codec/lib/types/common.ts
@@ -2,3 +2,15 @@ export type Location = "storage" | "memory" | "calldata";
 export type Visibility = "internal" | "external";
 export type Mutability = "pure" | "view" | "nonpayable" | "payable";
 export type ContractKind = "contract" | "library" | "interface";
+
+export class UnknownUserDefinedTypeError extends Error {
+  public typeString: string;
+  public id: string;
+  constructor(id: string, typeString: string) {
+    const message = `Cannot locate definition for ${typeString} (ID ${id})`;
+    super(message);
+    this.name = "UnknownUserDefinedTypeError";
+    this.id = id;
+    this.typeString = typeString;
+  }
+}
